4b4ad5736e93b1037f5f4ca44eb87ff57c72500d
githubqqqqq <catqqwhisper@gmail.com>
2019-07-19 13:51:02 -0400

    Add files via upload

---
+++ Pandas DataFrame Reformat Datetime Column.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+
+df['CD']=df["DATE"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
+##REF: http://strftime.org/
+
---
+++ Pandas DataFrame Rename Fields.py
@@ -0,0 +1,5 @@
+import pandas as pd
+    
+df = df.rename(columns={'OLDNAME1': 'NEWNAME1', 'OLDNAME2': 'NEWNAME2'})
+
+  
